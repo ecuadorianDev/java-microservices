d6a55cd62dc90680806d14a53c8f17ce03ab2112
ecuadorianDev <ec.com.developer@gmail.com>
2017-02-24 17:12:34 -0500

    Create new classes

---
+++ java-microservices/src/ec/com/developers/testers/HelloGitMac.java
@@ -0,0 +1,9 @@
+package ec.com.developers.testers;
+
+public class HelloGitMac {
+
+	public static void main(String[] args) {
+		System.out.println("Hello git from Mac");
+	}
+
+}
---
+++ java-microservices/src/ec/com/developers/testers/HelloGitWin7.java
@@ -0,0 +1,9 @@
+package ec.com.developers.testers;
+
+public class HelloGitWin7 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello git from win7");
+	}
+
+}
