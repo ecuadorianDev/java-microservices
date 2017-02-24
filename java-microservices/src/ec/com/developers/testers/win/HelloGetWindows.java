bd90ecbc1a7e8ada94b524cd80a420c57c9234f7
ecuadorianDev <ec.com.developer@gmail.com>
2017-02-24 17:18:35 -0500

    HelloGitWindows class Creation

---
+++ java-microservices/src/ec/com/developers/testers/win/HelloGetWindows.java
@@ -0,0 +1,9 @@
+package ec.com.developers.testers.win;
+
+public class HelloGetWindows {
+
+	public static void main(String[] args) {
+		System.out.println("Hello git from entire windows world");
+	}
+
+}
