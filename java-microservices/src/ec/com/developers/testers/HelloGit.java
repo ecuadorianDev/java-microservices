33d968c8de1f4096bcfdd121869899df77c93128
ecuadorianDev <ec.com.developer@gmail.com>
2017-02-24 16:52:23 -0500

    ad

--- java-microservices/src/ec/com/developers/testers/HelloGit.java
+++ java-microservices/src/ec/com/developers/testers/HelloGit.java
@@ -3,7 +3,7 @@ package ec.com.developers.testers;
 public class HelloGit {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Git from Ubuntu...");
+		System.out.println("Hello Git from Ubuntu...01");
 		System.out.println("Hello Git from Windows7Ultimate...");
 		System.out.println("Ubuntu: I'm doing rigth things...");
 		System.out.println("Congrats...");
